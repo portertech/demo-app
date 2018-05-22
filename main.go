bd6f55c88b7e05705a2bfe2ec504f8bd42525b7c
Sean Porter <portertech@gmail.com>
2018-05-22 12:26:56 -0700

    added statsd client to dummy app, request counters
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- main.go
+++ main.go
@@ -8,14 +8,28 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/quipo/statsd"
 )
 
 var healthy = true
 
 func main() {
+	prefix := "dummy."
+	statsdclient := statsd.NewStatsdClient("localhost:8125", prefix)
+	err := statsdclient.CreateSocket()
+	if nil != err {
+		log.Println(err)
+		os.Exit(1)
+	}
+	interval := time.Second * 10
+	stats := statsd.NewStatsdBuffer(interval, statsdclient)
+	defer stats.Close()
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		stats.Incr("http.requests.get.root", 1)
+
 		hostname, err := os.Hostname()
 
 		if err != nil {
@@ -26,6 +40,8 @@ func main() {
 	}).Methods(http.MethodGet)
 
 	r.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		stats.Incr("http.requests.get.healthz", 1)
+
 		if healthy {
 			w.Write([]byte("healthy"))
 		} else {
@@ -34,6 +50,8 @@ func main() {
 	}).Methods(http.MethodGet)
 
 	r.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		stats.Incr("http.requests.post.healthz", 1)
+
 		healthy = !healthy
 	}).Methods(http.MethodPost)
 
