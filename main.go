f5cdd756875805739d6c3b3a0365a21658c36e31
Sean Porter <portertech@gmail.com>
2018-05-23 13:01:18 -0500

    do not buffer statsd counters, let the agent do it
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- main.go
+++ main.go
@@ -15,20 +15,17 @@ var healthy = true
 
 func main() {
 	prefix := "dummy."
-	statsdclient := statsd.NewStatsdClient("localhost:8125", prefix)
+	statsdclient := statsd.NewStatsdClient("127.0.0.1:8125", prefix)
 	err := statsdclient.CreateSocket()
 	if nil != err {
 		log.Println(err)
 		os.Exit(1)
 	}
-	interval := time.Second * 10
-	stats := statsd.NewStatsdBuffer(interval, statsdclient)
-	defer stats.Close()
 
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		stats.Incr("http.requests.get.root", 1)
+		statsdclient.Incr("http.requests.get.root", 1)
 
 		hostname, err := os.Hostname()
 
@@ -40,7 +37,7 @@ func main() {
 	}).Methods(http.MethodGet)
 
 	r.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		stats.Incr("http.requests.get.healthz", 1)
+		statsdclient.Incr("http.requests.get.healthz", 1)
 
 		if healthy {
 			w.Write([]byte("healthy"))
@@ -50,7 +47,7 @@ func main() {
 	}).Methods(http.MethodGet)
 
 	r.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
-		stats.Incr("http.requests.post.healthz", 1)
+		statsdclient.Incr("http.requests.post.healthz", 1)
 
 		healthy = !healthy
 	}).Methods(http.MethodPost)
